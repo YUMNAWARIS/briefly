86d3a6a3da1c8d943bbd2846db0837ee53a865dd
Yumna Waris <YUMNAAHWARIS@GMAIL.COM>
2025-09-02 01:56:40 +0500

    fix

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import streamlit as st
 st.title("Briefly")
 
 def generate_response(input_text):
-    st.info("""hello""")
+    st.info(input_text)
 
 
 with st.form("summarize"):
     text = st.text_area(
         "Enter text to summarize - word limit 10,000",
-        """Elon Reeve Musk FRS (/lɒn/ EE-lon; born June 28, 1971) is an international businessman and entrepreneur known for his leadership of Tesla, SpaceX, X (formerly Twitter), and the Department of Government Efficiency (DOGE). """,
+        "",
     )
     submitted = st.form_submit_button("Submit")
     if submitted:
